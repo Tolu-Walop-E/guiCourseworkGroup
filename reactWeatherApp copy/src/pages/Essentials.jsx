73c350e2cb95db9894d59f4834884dc23f5ede33
Ehimen22510 <131770285+Ehimen22510@users.noreply.github.com>
2024-03-16 23:52:49 +0000

    Add files via upload

---
+++ reactWeatherApp copy/src/pages/Essentials.jsx
@@ -0,0 +1,17 @@
+import WeatherEss from './weatherEss';
+
+
+
+
+
+const Essentials = () => {  
+  return (
+    <div>
+    <h1>Weather Forecast App</h1> 
+    <WeatherEss/>
+    
+    </div> );
+};
+
+
+export default Essentials;
