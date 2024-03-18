87ebd4f0995f9b0f1190788443b8624b09830eed
Mobina Aghaeimaleki <120053168+mobina22am@users.noreply.github.com>
2024-03-18 13:25:06 +0000

    m

--- reactWeatherApp copy/src/pages/homePage.jsx
+++ reactWeatherApp copy/src/pages/homePage.jsx
@@ -1,17 +1,64 @@
 import React from 'react'
-import '../main.css'
-import SideBar from '../components/sideBar.jsx'
-import Temperature from '../components/temperature.jsx'
-import DayForecast from '../components/daysSection.jsx'
+import '../componentStyles/homepage.css'
+import setting from '../assets/settingsIcon.png'
+import list from '../assets/list.png'
+import cday from '../assets/clear_day.png'
+
 
 
 function HomePage(){
     return(
         <>
-            <home />
-            <Temperature backVisible={false} />
-            <DayForecast />
-            
+                    <div>
+                            <a id = "settings" href = "settings.html"><img src ={setting} id='setting'/></a>
+                            <a href="sidebar.html"><img src = {list} id = "sidebar"/></a>
+                      
+                    </div>
+
+                
+
+                    <div id="today">
+                        <p class="today" id="city">London</p>
+                        <p class="today" id="temp">18°</p>
+                    </div>
+
+
+                    <table id = "weekly">
+                        <tbody>
+                            <tr className = "days">
+                                <td className = "day">Monday</td>
+                                <td className = "icon"><img src = {cday}/></td>
+                                <td className = "unit">20°</td>
+                            </tr>
+                            <tr className = "days">
+                                    <td className = "day">Tuesday</td>
+                                    <td className = "icon"><img src = {cday}/></td>
+                                    <td className = "unit">18°</td>
+                            </tr>
+                            <tr className = "days">
+                                <td className = "day">Wednesday</td>
+                                <td className = "icon"><img src = {cday}/></td>
+                                <td className = "unit">20°</td>
+                            </tr>
+                            <tr className = "days">
+                                    <td className = "day">Thursday</td>
+                                    <td className = "icon"><img src = {cday}/></td>
+                                    <td className = "unit">19°</td>
+                            </tr>
+                            <tr className = "days">
+                                    <td className = "day">Friday</td>
+                                    <td className = "icon"><img src = {cday}/></td>
+                                    <td className = "unit">22°</td>
+                            </tr>
+                            <tr className = "days">
+                                <td className = "day">Saturday</td>
+                                <td className = "icon"><img src = {cday}/></td>
+                                <td className = "unit">23°</td>
+                            </tr>
+                        </tbody>
+                        
+                    </table>
+         
         </>
     )
 }
