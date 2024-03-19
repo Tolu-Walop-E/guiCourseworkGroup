e929f8b00b6e63626ac55c430e84082b91ed6da9
Ehimen22510 <131770285+Ehimen22510@users.noreply.github.com>
2024-03-19 20:55:40 +0000

    Update weatherEss.jsx

--- reactWeatherApp copy/src/components/weatherEss.jsx
+++ reactWeatherApp copy/src/components/weatherEss.jsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react'; 
-import "../componentStyles/Essentials.css"
-import { Link } from 'react-router-dom'
 import axios from 'axios';
 const WeatherEss = () => {
-    const [city, setCity] = useState('london');
     const [weatherData, setWeatherData] = useState(null);
     const fetchData = async () => { //fetches data from API
         try {
-            const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=7924099ca9b82c9a4fc854b7593c65f4`)
+            const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${localStorage.getItem("lat")}&lon=${localStorage.getItem("lng")}&appid=7924099ca9b82c9a4fc854b7593c65f4`)
             setWeatherData(response.data);
             console.log(response.data); //You can see all the weather data in console log
     } 
@@ -24,12 +21,14 @@ const WeatherEss = () => {
     let vis = 'visibility(';
     let rain = 'precipitation(';
     let win = 'wind speed(';
+    let cityname = '';
 
     if(weatherData !== null){ //ensures that the following code only works when the data is not null 
-        if (weatherData.list[0].main.temp > 35) { //if the tempurature is above 35 degrees celsius a recommendation is given
+        cityname = weatherData.city.name;
+        if (weatherData.list[0].main.temp > (localStorage.getItem("prefTemp")+ 7)) { //if the tempurature is above 35 degrees celsius a recommendation is given
             temp = temp + weatherData.list[0].main.temp + ' °C)' + 'is hot: wear clothing of minimal layers and bring water.';
         }
-        else if (weatherData.list[0].main.temp < 15){//if the tempurature is above 35 degrees celsius a recommendation is given
+        else if (weatherData.list[0].main.temp < (localStorage.getItem("prefTemp")- 7)){//if the tempurature is above 35 degrees celsius a recommendation is given
             temp = temp + weatherData.list[0].main.temp + ' °C)' + "is cold: wear extra layers of clothing.";
         }
         else{//anywhere in between is fine
@@ -50,14 +49,14 @@ const WeatherEss = () => {
             vis = vis + weatherData.list[0].visibility + ' m)'+ "is fine.";
         }
 
-        if (weatherData.list[0].pop > 0.5) { //compares the probabilty of precipitation value with a relative high value for pop
+        if ((weatherData.list[0].pop * 100) > ((localStorage.getItem("prefPrec") + 7))) { //compares the probabilty of precipitation value with a relative high value for pop
             rain = rain  + (weatherData.list[0].pop * 100) + '%)'+ 'is high: bring an umbrella, ride with extreme caution/take a different mode of transport';
         }
         else{
             rain  = rain  + (weatherData.list[0].pop * 100) + '%)' + "is low.";
         }
     
-        if (weatherData.list[0].wind.speed > 20) {//if wind speed is above 20 m/s a recommendation is given
+        if (weatherData.list[0].wind.speed > ((localStorage.getItem("prefWind") + 7))) {//if wind speed is above 20 m/s a recommendation is given
             win = win + weatherData.list[0].wind.speed + 'meter/sec)'+ 'is high: ride with caution';
         }
         else{
@@ -68,7 +67,7 @@ const WeatherEss = () => {
 
     return ( //button #got should link to homepage
         <section id = "recom">
-            <p>Essentials</p>
+            <p>Essentials for {cityname}</p>
             <ul>
                 <li>{temp}</li>
                 <li>{hum}</li>
@@ -77,7 +76,7 @@ const WeatherEss = () => {
                 <li>{win}</li>
             </ul>
 
-            <Link to = "/"><button id = "got">Got it</button></Link> 
+            <Link to "/"><button id = "got">Got it</button> </Link>
         </section>
     );
 };
